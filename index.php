39207be3a0ba1cd1f60f147502c305b2489bbbd3
gabriele131099 <gabrisanna131099@gmail.com>
2021-08-25 15:50:31 +0200

    php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+
+        <?php
+            $testo = 'Ciao sono Lucia, sono una sirena. Può sembrare strano ma è una storia vera';
+
+            echo {$testo};
+        ?>
+    <!--Creare una variabile con un paragrafo di testo a vostra scelta.
+    Stampare a schermo il paragrafo e la sua lunghezza.
+    Una parola da censurare viene passata dall’utente tramite parametro GET.
+    Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
+    Tutto ciò che volete provare in più nel vostro codice inseritelo in un file bonus.php-->
+    </main>
+</body>
+</html>
+
